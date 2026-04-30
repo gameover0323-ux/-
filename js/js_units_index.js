fa7d0fa35e524f2e0e16b7b57f391ec53c1f3d9c
gameover0323-ux <gameover0323@gmail.com>
2026-04-30 19:42:08 +0900

    Add strike_gundam to unitList and fix formatting

--- js/js_units_index.js
+++ js/js_units_index.js
@@ -2,7 +2,7 @@ import { gundam_mc } from "./js_units_gundam_mc.js";
 import { z_gundam } from "./js_units_z_gundam.js";
 import { shining_gundam } from "./js_units_shining_gundam.js";
 import { wing_zero } from "./js_units_wing_zero.js";
-
+import { strike_gundam } from "./js_units_strike_gundam.js";
 
 //ボス機体//
 import { devil_gundam } from "./js_units_devil_gundam.js";
@@ -11,9 +11,10 @@ export const unitList = [
   gundam_mc,
   z_gundam,
   shining_gundam,
-  wing_zero
+  wing_zero,
+  strike_gundam
 ];
 
-export const bossList = [ 
+export const bossList = [
   devil_gundam
-  ];
+];
