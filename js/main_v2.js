584b446de2e1d4e3e70b562e1fce494b49e5d441
gameover0323-ux <gameover0323@gmail.com>
2026-05-07 21:37:06 +0900

    Update main_v2.js

--- js/main_v2.js
+++ js/main_v2.js
@@ -11,6 +11,8 @@ import {
 
 import { onlineState } from "./js_online_state.js";
 import { unitList, bossList, cpuList, cpuBeginnerList } from "./js_units_index.js";
+alert(" import OK");
+/*
 import {
   createBattleState,
   applyUnitDerivedState,
@@ -31,8 +33,7 @@ import {
   executeUnitModifyTakenDamage,
   executeUnitModifyEvadeAttempt
 } from "./js_unit_runtime.js";
-alert(" import OK");
-/*
+
 import {
   takeHit as resolveTakeHit,
   evadeAttack as resolveEvadeAttack
