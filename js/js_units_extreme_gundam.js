0050b7eeff97f6ce4e1be40f9f90c9e97e4e8fbf
gameover0323-ux <gameover0323@gmail.com>
2026-05-07 17:26:29 +0900

    Add extreme_gundam object with initial properties

---
+++ js/js_units_extreme_gundam.js
@@ -0,0 +1,16 @@
+export const extreme_gundam = {
+  id: "extreme_gundam",
+  name: "エクストリームガンダム",
+  maxHp: 1000,
+  hp: 1000,
+  evade: 0,
+  evadeMax: 0,
+  isBoss: true,
+  formId: "normal",
+  defaultFormId: "normal",
+
+  slots: {},
+  slotOrder: [],
+  specialOrder: [],
+  specials: []
+};
