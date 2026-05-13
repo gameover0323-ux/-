f5822225e512b59ad51cb228341e0a29e315252a
gameover0323-ux <gameover0323@gmail.com>
2026-05-13 18:20:36 +0900

    Update main_v2.js

--- js/main_v2.js
+++ js/main_v2.js
@@ -2213,13 +2213,7 @@ document.getElementById("closePlayerStatsBtn")?.addEventListener("click", () => 
 loadUnitButtons();
 
 restorePlayerSession().then(() => {
-  if (playerSession.profile?.unlocks) {
-    extraUnlockedUnits = Object.entries(playerSession.profile.unlocks)
-      .filter(([, unlocked]) => unlocked)
-      .map(([unlockKey]) => UNLOCKABLE_UNIT_MAP[unlockKey])
-      .filter(Boolean);
-  }
-
+  syncExtraUnlockedUnitsFromProfile();
   updatePlayerCardUi();
   updateDebugButtonVisibility();
   bootOnlineFromUrl();
