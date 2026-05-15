0643c1ef055b8d87f50831699176c7995905fcd1
gameover0323-ux <gameover0323@gmail.com>
2026-05-16 06:54:24 +0900

    Create cpu_jegan_d_type from jegan_d_type

---
+++ js/js_units_cpu_jegan_d_type.js
@@ -0,0 +1,6 @@
+import { jegan_d_type } from "./js_units_jegan_d_type.js";
+
+export const cpu_jegan_d_type = structuredClone(jegan_d_type);
+
+cpu_jegan_d_type.id = "cpu_jegan_d_type";
+cpu_jegan_d_type.name = "ジェガンD型";
